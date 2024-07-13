a0b27f124ee137c7e7732a88190535cf4ef0ae34
prawesh jha <erprawesh@gmail.com>
2024-07-13 13:23:22 +0530

    new-change-13-7-2024

--- src/ArticleList.jsx
+++ src/ArticleList.jsx
@@ -1,46 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./ArticleList.css";
 
-const ArticleList = ({ articles }) => (
-  <>
-    <div className="row app">
-      <div className="container mt-4 mb-5" id="blog">
-        <h2 className="text-capitalize text-center mt-2">My Blog</h2>
-      </div>
-      {articles.map((article, key) => (
-        <div className="col-md-4 mb-5">
-          <div className="newcard">
-            <Link className="newcard" key={key} to={`/blog/${article.name}`}>
-              <div className="newcard-header">
-                <img
-                  className="img-fluid"
-                  alt="{article.title}"
-                  src={article.img}
-                />
-              </div>
-              <div className="newcard-body">
-                <span className="tag tag-blue">Technology</span>
-                <h4>{article.title}</h4>
-                <p>{article.content[0].substring(0, 100)}...</p>
-                <div className="user">
-                  <img src="/src/assets/image/loginimg.png" alt="" />
-                  <div className="user-info">
-                    <h5>
-                      <a href={article.authorlink} class="text-decoration-none">
-                        {article.author}
-                      </a>
-                    </h5>
-                    <small> {article.date}</small>
+const ArticleList = ({ articles }) => {
+  const [visibleCount, setVisibleCount] = useState(6);
+
+  const loadMore = () => {
+    setVisibleCount((prevCount) => Math.min(prevCount + 6, articles.length));
+  };
+
+  const loadLess = () => {
+    setVisibleCount((prevCount) => Math.max(prevCount - 6, 6));
+  };
+
+  return (
+    <>
+      <div className="row">
+        <div className="container mt-4 mb-5" id="blog">
+          <h2 className="text-capitalize text-center mt-2">My Blog</h2>
+        </div>
+        {articles.slice(0, visibleCount).map((article, key) => (
+          <div className="col-md-4 mb-5" key={key}>
+            <div className="newcard">
+              <Link className="newcard" to={`/blog/${article.name}`}>
+                <div className="newcard-header">
+                  <img
+                    className="img-fluid"
+                    alt="{article.title}"
+                    src={article.img}
+                  />
+                </div>
+                <div className="newcard-body">
+                  <span className="tag tag-blue">Technology</span>
+                  <h4>{article.title}</h4>
+                  <p>{article.content[0].substring(0, 100)}...</p>
+                  <div className="user">
+                    <img src="/src/assets/image/loginimg.png" alt="" />
+                    <div className="user-info">
+                      <h5>
+                        <a
+                          href={article.authorlink}
+                          className="text-decoration-none"
+                        >
+                          {article.author}
+                        </a>
+                      </h5>
+                      <small>{article.date}</small>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  </>
-);
+        ))}
+      </div>
+      <div className="mb-5">
+        {(visibleCount < articles.length || visibleCount > 6) && (
+          <div className="text-center">
+            {visibleCount < articles.length && (
+              <Link to="/Blog">
+                <button className="btn btn-primary mr-2" onClick={loadMore}>
+                  Load More
+                </button>
+              </Link>
+            )}
+            {visibleCount > 6 && (
+              <button className="btn btn-secondary" onClick={loadLess}>
+                Load Less
+              </button>
+            )}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default ArticleList;
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,78 +1,70 @@
 import React, { useState } from "react";
 import { Offcanvas, OffcanvasHeader, OffcanvasBody } from "react-bootstrap";
+import "./Navigation.css";
 
+// The Navigation component
 const Navigation = () => {
+  // State to manage the visibility of the Offcanvas menu
   const [showOffcanvas, setShowOffcanvas] = useState(false);
+  // State to keep track of the currently active link
+  const [activeLink, setActiveLink] = useState("/");
+  // Function to close the Offcanvas menu
   const handleClose = () => setShowOffcanvas(false);
+  // Function to show the Offcanvas menu
   const handleShow = () => setShowOffcanvas(true);
+
+  // Function to set the active link and close the Offcanvas menu
+  const handleSetActiveLink = (link) => {
+    setActiveLink(link);
+    setShowOffcanvas(false); // Close the Offcanvas menu when a link is clicked
+  };
+
+  // Navigation items
+  const navItems = [
+    { link: "/", label: "Home" },
+    { link: "#aboutme", label: "About" },
+    { link: "#service", label: "Service" },
+    { link: "#portfolio", label: "Portfolio" },
+    { link: "#blog", label: "Blog" },
+    { link: "#contactus", label: "Contact" },
+  ];
+
+  // Helper function to render a navigation item
+  const renderNavItem = (link, label) => (
+    <li
+      className={`nav-item ${activeLink === link ? "active" : ""}`}
+      key={link}
+    >
+      <a href={link} onClick={() => handleSetActiveLink(link)}>
+        {label}
+      </a>
+    </li>
+  );
 
   return (
     <nav className="navbar navbar-expand-lg" id="navbar_top">
       <div className="container">
+        {/* Logo */}
         <img
           src="/src/assets/image/jha.png"
           alt="logo"
           width="80"
           className="main-logo"
         />
+
+        {/* Button to show Offcanvas menu on small screens */}
         <button className="btn d-lg-none" type="button" onClick={handleShow}>
           <i className="fa-solid fa-bars-staggered fs-1"></i>
         </button>
-        <Offcanvas
-          show={showOffcanvas}
-          onHide={handleClose}
-          placement="end"
-          className="w-50"
-        >
-          <OffcanvasHeader className="justify-content-end">
-            <button
-              type="button"
-              className="btn-close"
-              aria-label="Close"
-              onClick={handleClose}
-            ></button>
-          </OffcanvasHeader>
-          <OffcanvasBody className="justify-content-md-center">
-            <ul className="navbar-nav text-center">
-              <li className="nav-item">
-                <a
-                  className="nav-link active text-dark"
-                  aria-current="page"
-                  href="/"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-dark" href="#aboutme">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-dark" href="#service">
-                  Service
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-dark" href="#portfolio">
-                  Portfolio
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-dark" href="#blog">
-                  Blog
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-dark" href="#contactus">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </OffcanvasBody>
-        </Offcanvas>
-        <div className="d-none d-lg-block">
-          <section>
+
+        {/* Navigation items for large screens */}
+        <div className="collapse navbar-collapse d-none d-lg-block container">
+          <ul className="navbar-nav mx-auto">
+            {navItems.map((item) => renderNavItem(item.link, item.label))}
+          </ul>
+
+          {/* Social media links */}
+          <section className="d-lg-block d-none">
             <a
               className="btn text-white btn-floating m-1"
               style={{ backgroundColor: "#ac2bac" }}
@@ -99,6 +91,28 @@ const Navigation = () => {
             </a>
           </section>
         </div>
+
+        {/* Offcanvas menu for small screens */}
+        <Offcanvas
+          show={showOffcanvas}
+          onHide={handleClose}
+          placement="end"
+          className="w-50"
+        >
+          <OffcanvasHeader className="justify-content-end">
+            <button
+              type="button"
+              className="btn-close"
+              aria-label="Close"
+              onClick={handleClose}
+            ></button>
+          </OffcanvasHeader>
+          <OffcanvasBody className="justify-content-md-center container">
+            <ul className="navbar-nav text-center">
+              {navItems.map((item) => renderNavItem(item.link, item.label))}
+            </ul>
+          </OffcanvasBody>
+        </Offcanvas>
       </div>
     </nav>
   );
