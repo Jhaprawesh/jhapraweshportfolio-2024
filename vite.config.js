439d03871d8ec33e5421c2aaad6d5844e02f07d1
prawesh jha <erprawesh@gmail.com>
2024-07-25 12:18:39 +0530

    new config file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.md"],
+  resolve: {
+    alias: {
+      "#minpath": path.resolve(__dirname, "node_modules/vfile/lib/index.js"),
+    },
+  },
   build: {
     rollupOptions: {
       external: [/^node:.*/],
