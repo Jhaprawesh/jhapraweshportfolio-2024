065acd5c7d2dc0b10e496aa4504867df2a9254f9
prawesh jha <erprawesh@gmail.com>
2024-09-16 01:09:13 +0530

    bundi-update

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,117 +1,132 @@
 import React from "react";
 import contImage from "../assets/image/bg.jpg";
+import Title from "./Title";
 
 const ContactUs = () => {
   return (
-    <div className="container" id="contactus">
-      <h2 className="text-capitalize text-center mt-2">Get in touch</h2>
-      <div className="row position-relative mt-5" data-aos="zoom-in">
-        <div className="col-md-6">
-          <div className="image">
-            <img
-              src={contImage}
-              className="carou img-thumbnail"
-              alt="Contact background"
-            />
-          </div>
-          <div className="text_info g-3">
-            <div className="location col-md-4 col-sm-4 p-3">
-              <i className="fa-solid fa-location-dot text-success">Location</i>
-              <span className="text-light"> India, Bihar</span>
-            </div>
-            <div className="phone col-md-4 col-sm-4 p-3">
-              <i className="fa-sharp fa-solid fa-phone text-success">
-                Let's Talk
-              </i>
-              <span className="text-light">7981379168</span>
-            </div>
-            <div className="email col-md-4 col-sm-4 p-3">
-              <i className="fa-solid fa-envelope text-success">
-                General Support
-              </i>
-              <span className="text-light">erprawesh@gmail.com</span>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <form action="https://formspree.io/f/mwkzolwj" method="POST">
-            <div>
-              <p className="text-center">Send Us A Message</p>
-            </div>
-            <div className="row g-3">
-              <span>Name</span>
-              <div className="col">
-                <input
-                  type="text"
-                  name="name"
-                  className="form-control"
-                  placeholder="First name"
-                  aria-label="First name"
-                  required
-                />
-              </div>
-              <div className="col">
-                <input
-                  type="text"
-                  name="lastname"
-                  className="form-control"
-                  placeholder="Last name"
-                  aria-label="Last name"
-                  required
-                />
-              </div>
-            </div>
-
-            <div className="mb-3">
-              <label htmlFor="exampleFormControlInput1" className="form-label">
-                Email address
-              </label>
-              <input
-                type="email"
-                name="email"
-                className="form-control"
-                id="exampleFormControlInput1"
-                placeholder="name@example.com"
-                required
+    <>
+      <div className="container" id="contactus">
+        <Title
+          title="Contact-Us"
+          subTitle="Or Reach Us At"
+          titleClassName={"tw-text-blue-950"}
+          subtitleClassName={"tw-text-950"}
+        />
+        <div className="row position-relative mt-5" data-aos="zoom-in">
+          <div className="col-md-6">
+            <div className="image">
+              <img
+                src={contImage}
+                className="carou img-thumbnail"
+                alt="Contact background"
               />
             </div>
-
-            <div className="mb-3">
-              <label htmlFor="exampleFormControlInput1" className="form-label">
-                Phone Number
-              </label>
-              <input
-                type="text"
-                name="phone"
-                className="form-control"
-                id="exampleFormControlInput1"
-                placeholder="+91 79XXXXXXXX"
-                required
-              />
+            <div className="text_info g-3">
+              <div className="location col-md-4 col-sm-4 p-3">
+                <i className="fa-solid fa-location-dot text-success">
+                  Location
+                </i>
+                <span className="text-light"> India, Bihar</span>
+              </div>
+              <div className="phone col-md-4 col-sm-4 p-3">
+                <i className="fa-sharp fa-solid fa-phone text-success">
+                  Let's Talk
+                </i>
+                <span className="text-light">7981379168</span>
+              </div>
+              <div className="email col-md-4 col-sm-4 p-3">
+                <i className="fa-solid fa-envelope text-success">
+                  General Support
+                </i>
+                <span className="text-light">erprawesh@gmail.com</span>
+              </div>
             </div>
+          </div>
+          <div className="col-md-6">
+            <form action="https://formspree.io/f/mwkzolwj" method="POST">
+              <div className="row g-3 mb-4">
+                <div className="col">
+                  <label htmlFor="name" className="form-label">
+                    <span>Name</span>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="form-control"
+                      placeholder="First name"
+                      aria-label="First name"
+                      required
+                    />
+                  </label>
+                </div>
+                <div className="col">
+                  <label htmlFor="lastname" className="form-label">
+                    <span>Last Name</span>
+                    <input
+                      type="text"
+                      name="lastname"
+                      id="lastname"
+                      className="form-control"
+                      placeholder="Last name"
+                      aria-label="Last name"
+                      required
+                    />
+                  </label>
+                </div>
+              </div>
 
-            <div className="mb-3">
-              <label
-                htmlFor="exampleFormControlTextarea1"
-                className="form-label"
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label w-100">
+                  Email address
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="form-control"
+                    placeholder="name@example.com"
+                    required
+                  />
+                </label>
+              </div>
+
+              <div className="mb-3 ">
+                <label htmlFor="phone" className="form-label w-100">
+                  Phone Number
+                  <input
+                    type="text"
+                    name="phone"
+                    id="phone"
+                    className="form-control"
+                    placeholder="+91 79XXXXXXXX"
+                    required
+                  />
+                </label>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="message" className="form-label w-100">
+                  Message
+                  <textarea
+                    className="form-control"
+                    name="message"
+                    id="message"
+                    rows="3"
+                    placeholder="Your message here..."
+                  ></textarea>
+                </label>
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary text-center w-100"
               >
-                Message
-              </label>
-              <textarea
-                className="form-control"
-                name="message"
-                id="exampleFormControlTextarea1"
-                rows="3"
-              ></textarea>
-            </div>
-
-            <button type="submit" className="btn btn-primary text-center w-100">
-              Submit
-            </button>
-          </form>
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import cssIcon from "../assets/image/svg/css-svgrepo-com.svg";
 import figmaIcon from "../assets/image/svg/figma-svgrepo-com.svg";
 import htmlIcon from "../assets/image/svg/html-5-svgrepo-com.svg";
-import cssIcon from "../assets/image/svg/css-svgrepo-com.svg";
 import javascriptIcon from "../assets/image/svg/javascript-svgrepo-com.svg";
-import reactIcon from "../assets/image/svg/react-svgrepo-com.svg";
+import mongoIcon from "../assets/image/svg/mongo-svgrepo-com.svg";
+import nodeIcon from "../assets/image/svg/node-svgrepo-com.svg";
 import routerIcon from "../assets/image/svg/react-router-svgrepo-com.svg";
+import reactIcon from "../assets/image/svg/react-svgrepo-com.svg";
 import tailwindIcon from "../assets/image/svg/tailwind-svgrepo-com.svg";
+import expressIcon from "../assets/image/svg/icons8-express-js.svg";
+import gitIcon from "../assets/image/svg/github-142-svgrepo-com.svg";
 
 const skillComponent = [
   {
@@ -25,7 +29,7 @@ const skillComponent = [
   },
   {
     id: "3",
-    title: "Javascript",
+    title: "JavaScript",
     Images: javascriptIcon,
   },
   {
@@ -35,31 +39,54 @@ const skillComponent = [
   },
   {
     id: "5",
-    title: "React-router",
+    title: "React Router",
     Images: routerIcon,
+  },
+  {
+    id: "6",
+    title: "Tailwind",
+    Images: tailwindIcon,
+  },
+  {
+    id: "7",
+    title: "Node.js",
+    Images: nodeIcon,
+  },
+  {
+    id: "8",
+    title: "ExpressJS",
+    Images: expressIcon,
+  },
+  {
+    id: "9",
+    title: "MongoDB",
+    Images: mongoIcon,
+  },
+  {
+    id: "10",
+    title: "GitHub",
+    Images: gitIcon,
   },
   // Add more unique skills here
 ];
 
 function Skills() {
   return (
-    <>
-      <div
-        className="tw-mt-8 tw-grid tw-grid-cols-3 tw-gap-6 md:tw-grid-cols-4 lg:tw-grid-cols-5"
-        id="frameworks-integration"
-      >
-        {skillComponent.map((item, index) => (
-          <div
-            key={index}
-            className="tw-grid tw-w-full tw-min-w-[3rem] tw-transform tw-cursor-pointer tw-place-items-center tw-rounded-xl tw-border tw-border-blue-gray-50 tw-bg-white tw-px-3  tw-transition-all hover:tw-scale-105 hover:tw-border-blue-gray-100 hover:tw-bg-blue-gray-50 hover:tw-bg-opacity-25"
-          >
-            <span className="tw-my-6 tw-grid tw-h-24 tw-w-24 tw-place-items-center">
-              <img src={item.Images} alt="" />
-            </span>
-          </div>
-        ))}
-      </div>
-    </>
+    <div
+      className="tw-mt-8 tw-grid tw-grid-cols-3 tw-gap-6 md:tw-grid-cols-4 lg:tw-grid-cols-5"
+      id="frameworks-integration"
+    >
+      {skillComponent.map((item) => (
+        <div
+          key={item.id}
+          className="tw-grid tw-w-full tw-min-w-[3rem] tw-transform tw-cursor-pointer tw-place-items-center tw-rounded-xl tw-border tw-border-blue-gray-50 tw-bg-white tw-px-3 tw-py-2 tw-transition-all hover:tw-scale-105 hover:tw-border-blue-gray-100 hover:tw-bg-blue-gray-50 hover:tw-bg-opacity-25"
+        >
+          <span className="tw-my-6 tw-grid tw-h-24 tw-w-24 tw-place-items-center">
+            <img src={item.Images} alt={`${item.title} logo`} />
+          </span>
+        </div>
+      ))}
+    </div>
   );
 }
 
