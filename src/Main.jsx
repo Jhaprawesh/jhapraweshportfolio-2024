1502f14de574d17c9fe43bb52b565e8977e8525f
prawesh jha <erprawesh@gmail.com>
2024-11-13 14:04:46 +0530

    fixed-bugs

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,11 +1,11 @@
+import "@mantine/core/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./styles/NewGlobal.css";
+import App from "./App";
 import ThemeProvider from "./context/ThemeContext";
 import "./styles/index.css";
-import "@mantine/core/styles.css";
+import "./styles/NewGlobal.css";
 
 import { MantineProvider } from "@mantine/core";
 
